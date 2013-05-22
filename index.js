6593419d7080b607fe9f2ab0a1c58fc0105b977b
Tingan Ho <tingan87@gmail.com>
2013-05-23 01:59:59 +0800

    Fixed autoroute

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 
-var grunt = require('grunt');
+var grunt = require('grunt'),
+    path  = require('path');
 
-module.exports = function(app, files) {
-  var files = grunt.file.expand({filter: 'isFile'}, files);
-  for(var i = 0; i < files.length; i++) {
-    require(files[i])(app);
+module.exports = function(files, app) {
+  var caller    = getCaller(),
+      callerDir = path.dirname(caller.filename);
+
+  var _files =
+    grunt.file.expand({
+      cwd: callerDir,
+      filter: 'isFile'
+    },
+      files
+    );
+
+  for(var i = 0; i < _files.length; i++) {
+    require(path.join(callerDir, _files[i]))(app);
   }
 };
+
+function getCaller() {
+  var stack = getStack()
+
+  // Remove superfluous function calls on stack
+  stack.shift() // getCaller --> getStack
+  stack.shift() // omfg --> getCaller
+
+  // Return caller's caller
+  return stack[1].receiver
+}
+
+function getStack() {
+  // Save original Error.prepareStackTrace
+  var origPrepareStackTrace = Error.prepareStackTrace
+
+  // Override with function that just returns `stack`
+  Error.prepareStackTrace = function (_, stack) {
+    return stack
+  }
+
+  // Create a new `Error`, which automatically gets `stack`
+  var err = new Error()
+
+  // Evaluate `err.stack`, which calls our new `Error.prepareStackTrace`
+  var stack = err.stack
+
+  // Restore original `Error.prepareStackTrace`
+  Error.prepareStackTrace = origPrepareStackTrace
+
+  // Remove superfluous function call on stack
+  stack.shift() // getStack --> Error
+
+  return stack
+}
